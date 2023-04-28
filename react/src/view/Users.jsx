1eb1aad6fcde41bf40f581a5129fc2fa9036e350
NataliaVer <33368867+NataliaVer@users.noreply.github.com>
2023-04-28 15:34:38 +0300

    use pagination

--- react/src/view/Users.jsx
+++ react/src/view/Users.jsx
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 import axiosClient from "../axios-client";
 import { useStateContext } from "../contexts/ContextProvider";
+import Pagination from "react-js-pagination";
 
 export default function Users() {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(false);
-    const {setNotification} = useStateContext()
+    const {setNotification} = useStateContext();
+    const [ pages, setPages ] = useState({
+        total: null,
+        current_page: 1,
+        per_page: null,
+    });
 
     useEffect(() => {
         getUsers();
@@ -24,13 +30,16 @@ export default function Users() {
             })
     }
 
-    const getUsers = () => {
+    const getUsers = (pageNumber=1) => {
+        console.log(pageNumber);
         setLoading(true)
-        axiosClient.get('/users')
+        axiosClient.get(`/users?page=${pageNumber}`)
             .then(({data}) => {
+                setPages(data.meta);
                 setLoading(false);
-                // console.log(data);
+                console.log(data);
                 setUsers(data.data);
+
             })
             .catch(() => {
                 setLoading(false)
@@ -64,11 +73,11 @@ export default function Users() {
                     }
                     {!loading && <tbody>
                         {users.map(u => (
-                            <tr>
+                            <tr key={u.id}>
                                 <td>{u.id}</td>
                                 <td>{u.name}</td>
                                 <td>{u.email}</td>
-                                <td>{u.crqated_at}</td>
+                                <td>{u.created_at}</td>
                                 <td>
                                     <Link to={'/users/'+u.id} className="btn-edit">Edit</Link>
                                     &nbsp;
@@ -78,6 +87,18 @@ export default function Users() {
                         ))}
                     </tbody>}
                 </table>
+                <div>
+                <Pagination
+                    activePage={pages.current_page}
+                    totalItemsCount={pages.total}
+                    itemsCountPerPage={pages.per_page}
+                    onChange={ev => getUsers(ev)}
+                    itemClass="page-item"
+                    linkClass="page-link"
+                    firstPageText="First"
+                    lastPageText="last"
+                />
+                </div>
             </div>
         </div>
     )
